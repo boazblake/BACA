5abeb057c1df3a28694b4e4573a54cf8f3f571ee
Boaz Blake <BBlake@goempyrean.com>
2021-06-29 17:14:13 -0500

    getting styli wit it

--- app/Layouts/main.js
+++ app/Layouts/main.js
@@ -4,8 +4,8 @@ const Main = () => {
       m(
         "main",
         m(
-          "section.is-marginless.bg-primary.card.text-white",
-          m("h1#page-title.is-center.bold", mdl.state.route.name)
+          "section#page-title.is-marginless.bg-primary.card.text-white",
+          m("h1.is-center.bold", mdl.state.route.name)
         ),
         children
       ),
