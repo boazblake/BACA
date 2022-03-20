0be9e0ff9c80243cc3dab32fe0a0343f4ad4514d
Boaz Blake <BBlake@goempyrean.com>
2022-03-20 07:36:44 -0500

    build

--- docs/sw.js
+++ docs/sw.js
@@ -30,8 +30,16 @@ self.__precacheManifest = [
     "revision": "491d81df15e317315289fc39396f458c"
   },
   {
+    "url": "app.css.gz",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
     "url": "app.js",
     "revision": "51f565dc28452b1474f0942bb391e5a7"
+  },
+  {
+    "url": "app.js.gz",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
   },
   {
     "url": "files/Bonham_Acres_Sec_1_Amendent_3.pdf",
@@ -182,8 +190,16 @@ self.__precacheManifest = [
     "revision": "94db90297296cb11e1ef2ae27ef06cc4"
   },
   {
+    "url": "index.html.gz",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
     "url": "manifest.json",
     "revision": "66fba0463b7f8a94b44aef864ba46875"
+  },
+  {
+    "url": "vendor.js.gz",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
