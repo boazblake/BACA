aed6bc78cfc20aca687f2a2c3fcea679b7036ea0
bblake <bblake@goempyrean.com>
2023-06-02 17:09:58 -0500

    for real

--- app/Pages/Account/dues.js
+++ app/Pages/Account/dues.js
@@ -1,25 +1,31 @@
 import m from "mithril"
 import PayPal from "./paypal"
 import { Table, formatDataForTable } from "@/Components/table.js"
+import { propEq } from "ramda"
 
 const state = {
   paypal: null,
 }
 
-const Dues = {
-  view: ({ attrs: { mdl, reload } }) =>
-    m(
-      "#DUES.section.p-y-50",
-      m(PayPal, { mdl, data: mdl.dues, status: state, reload }),
-      mdl.dues.any() &&
-      m(Table, {
-        mdl,
-        data: formatDataForTable(
-          ["userId", "objectId", "createdAt", "updatedAt"],
-          mdl.dues
-        ),
-      })
-    ),
+const Dues = ({ attrs: { mdl } }) => {
+  const fetchDues = (mdl) => mdl.http.back4App.getTask(mdl)(`dues/${mdl.user.objectId}`).fork(log('e'), mdl.dues = dues)
+  fetchDues(mdl)
+
+  return {
+    view: ({ attrs: { mdl } }) =>
+      m(
+        "#DUES.section.p-y-50",
+        m(PayPal, { mdl, data: mdl.dues, status: state, reload: fetchDues }),
+        mdl.dues.any() &&
+        m(Table, {
+          mdl,
+          data: formatDataForTable(
+            ["userId", "objectId", "createdAt", "updatedAt"],
+            mdl.dues
+          ),
+        })
+      ),
+  }
 }
 
 export default Dues
--- app/Pages/Auth/fns.js
+++ app/Pages/Auth/fns.js
@@ -4,7 +4,7 @@ import { prop } from 'ramda'
 const addPayPal = () => {
   window.paypal = null
   const script = document.createElement('script')
-  script.src = "https://www.paypal.com/sdk/js?client-id=ASs3FDsWTORwsfyCIoG4DSgm0ReuA2&components=buttons&enable-funding=venmo,paylater&currency=USD"
+  script.src = "https://www.paypal.com/sdk/js?client-id=ASs3FDsWTORwsfyCIoG4DSgm0ReuA2jQpU9A_2u9F-LMhSl0i3H3na6iJ6GS_tuHefm4y4-EY2JCAKAN&components=buttons&enable-funding=venmo,paylater&currency=USD"
   script.id = 'paypal'
   script.async = true
   script.defer = true
